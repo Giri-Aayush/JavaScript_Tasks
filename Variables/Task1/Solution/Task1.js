f33cb79bb2fc79294185b4c79cd5a77a702df5d4
Aayush Giri <aayushgiri1234@gmail.com>
2022-10-19 14:40:00 +0530

    third commit

---
+++ Variables/Task1/Solution/Task1.js
@@ -0,0 +1,12 @@
+//Declare two variables: Admin and Name.
+let Admin;
+let Name;
+
+//Assign the value "John" to name.
+Name = 'John';
+
+//Copy the value from name to admin.
+Admin = Name;
+
+//Show the value of admin using alert (must output “John”).
+alert(Admin);
---
+++ Variables/Task3/Task3.js
@@ -0,0 +1,12 @@
+//Examine the following code:
+
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+//Would it be right to use upper case for birthday? For age? Or even for both?
+/* NOTE:
+ The age is calculated from birthday using someCode(), which means a function call
+ that we didn’t explain yet (we will soon!), but the details don’t matter here, the point 
+ is that age is calculated somehow based on the birthday.
+ */

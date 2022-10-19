397f1974be2e29d0757e0dc37e664db0b27d82a0
Aayush Giri <aayushgiri1234@gmail.com>
2022-10-19 14:01:13 +0530

    second commit

---
+++ Hello, world!/Task1_2/Solution/alert.js
@@ -0,0 +1,8 @@
+//step 1: always is to check if the js file has been attached correctly
+//use simple console.log command
+
+console.log("checking the connection")
+
+//now perform the task
+
+alert("I'm JavaScript!")

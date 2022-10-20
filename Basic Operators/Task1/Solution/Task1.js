3b5dd53d621be0faa0b6e25dc34273ad38659f8c
Aayush Giri <aayushgiri1234@gmail.com>
2022-10-20 10:38:05 +0530

    Sixth Commit

---
+++ Basic Operators/Task1/Solution/Task1.js
@@ -0,0 +1,7 @@
+let a = 1, b = 1;
+
+let c = ++a; // 2 , first increment happens and then assignment happens
+let d = b++; // 1 , first assignment happens and then increment happens
+
+//the value of a after these lines is 2
+//the value of b after these lines is 2
---
+++ Basic Operators/Task2/Solution/Task2.js
@@ -0,0 +1,6 @@
+let a = 2;
+//(a*=2) this will return the value of a after completing the operation
+
+let x = 1 + (a *= 2);
+// the value of x is 5
+alert(x);
